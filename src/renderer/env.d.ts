c15828f208f185e1d407f3764a4ea97d78074427
miket-llc <mdt_@msn.com>
2024-12-10 21:18:06 -0500

    unix line endings

--- src/renderer/env.d.ts
+++ src/renderer/env.d.ts
@@ -1,15 +1,11 @@
 /// <reference types="vite/client" />
-
 import { ElectronAPI } from '..common/types/types'
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-
 declare global {
   interface Window {
     electron: ElectronAPI & {
@@ -18,7 +14,6 @@ declare global {
       }
     }
   }
-
   interface Device {
     id: string
     name: string
